f77e44c5c0d1411f0ec9d9fa65dea6dda2055994
DarthHaran <jaksasith@gmail.com>
2021-01-21 18:53:31 +0100

    Dir struct changed and initial Angular files added

--- Entities/Project.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GRT.Entities
-{
-    public class Project
-    {
-        public int Id { get; set; }
-        public string ProjectName { get; set; }
-        public string Domain { get; set; }
-    }
-}
