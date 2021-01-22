c1d391d2fb3b7323d239d261733b5760d2fa9586
DarthHaran <jaksasith@gmail.com>
2021-01-22 20:07:30 +0100

    Sidebar added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { SideComponent } from './side/side.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule,
