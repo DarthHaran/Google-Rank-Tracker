ee9ec6c494c598ef6d315d435cf5b7c6fd91499a
DarthHaran <jaksasith@gmail.com>
2021-01-22 16:30:56 +0100

    Enabled GET api/projects/ requests in Angular app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Google Rank Tracker';
+  projects: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getProjects();
+  }
+
+  getProjects() {
+    this.http.get('http://localhost:5000/api/projects').subscribe(response => {
+      this.projects = response;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
