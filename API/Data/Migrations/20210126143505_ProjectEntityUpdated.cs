f82e51296db3d53ff3c9212887d7a2e61fc5e981
DarthHaran <jaksasith@gmail.com>
2021-01-26 15:41:20 +0100

    DateTime property added to Project entity

---
+++ API/Data/Migrations/20210126143505_ProjectEntityUpdated.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRT.Data.Migrations
+{
+    public partial class ProjectEntityUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAdded",
+                table: "Projects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAdded",
+                table: "Projects");
+        }
+    }
+}
--- API/Entities/Project.cs
+++ API/Entities/Project.cs
@@ -1,9 +1,12 @@
-﻿namespace GRT.Entities
+﻿using System;
+
+namespace GRT.Entities
 {
     public class Project
     {
         public int Id { get; set; }
         public string ProjectName { get; set; }
         public string Domain { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
