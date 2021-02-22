c6b70e1471c97dff2506051e70423173c32c3d34
DarthHaran <jaksasith@gmail.com>
2021-02-22 17:35:31 +0100

    Routing updated

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddprojectComponent } from './addproject/addproject.component';
+import { KeywordsComponent } from './keywords/keywords.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-  {path: 'addproject', component: AddprojectComponent}
+  { path: '', pathMatch: 'full', redirectTo: 'projects' },
+  {path: 'addproject', component: AddprojectComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'keywords', component: KeywordsComponent},
+  {path: 'keywords/:id', component: KeywordsComponent}
 ];
 
 @NgModule({
